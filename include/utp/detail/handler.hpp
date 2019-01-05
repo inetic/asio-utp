496b5e63faf166b586dd4906cf0c2fc3c8a457ae
Peter Jankuliak <p.jankuliak@gmail.com>
2019-01-05 13:23:34 +0100

    Implement impl/handler using vtable

--- include/utp/detail/handler.hpp
+++ include/utp/detail/handler.hpp
@@ -13,16 +13,21 @@ private:
     typedef void(*exec_type)(void*, const error_code&, Args...);
     typedef void(*destruct_type)(void*);
 
-    template<class Func>
-    struct operation {
-        static void exec(void* data, const error_code& ec, Args... args)
-        {
-            (*reinterpret_cast<Func*>(data))(ec, args...);
-        }
+    struct base {
+        virtual void exec(const error_code&, Args...) = 0;
+        virtual ~base() {};
+    };
 
-        static void destruct(void* data)
+    template<class Func>
+    struct impl final : public base {
+        Func f;
+
+        template<class F>
+        impl(F&& f) : f(std::forward<F>(f)) {}
+
+        void exec(const error_code& ec, Args... args) override
         {
-            reinterpret_cast<Func*>(data)->~Func();
+            f(ec, args...);
         }
     };
 
@@ -31,62 +36,27 @@ public:
 
     handler(const handler&) = delete;
 
-    handler(handler&& h)
-        : _exec(h._exec)
-        , _destruct(h._destruct)
-        , _func_data(std::move(h._func_data))
-    {
-        h._exec = nullptr;
-        h._destruct = nullptr;
-    }
+    handler(handler&& h) : _impl(std::move(h._impl)) { }
 
     template<class Func> handler(Func&& func)
     {
-        _exec = operation<Func>::exec;
-        _destruct = operation<Func>::destruct;
-
-        _func_data.resize(sizeof(Func));
-        new (_func_data.data()) Func(std::forward<Func>(func));
-    }
-
-    template<class Func> void operator=(Func&& func)
-    {
-        if (_func_data.size()) {
-            _destruct(_func_data.data());
-        }
-
-        _exec = operation<Func>::exec;
-        _destruct = operation<Func>::destruct;
-
-        _func_data.resize(sizeof(Func));
-        new (_func_data.data()) Func(std::forward<Func>(func));
+        _impl.reset(new impl<Func>(std::forward<Func>(func)));
     }
 
     void operator()(const error_code& ec, Args... args)
     {
-        _exec(_func_data.data(), ec, args...);
+        _impl->exec(ec, args...);
     }
 
     void operator()(const error_code& ec, Args... args) const
     {
-        _exec((void*) _func_data.data(), ec, args...);
+        _impl->exec(ec, args...);
     }
 
-    operator bool() const {
-        return _func_data.size();
-    }
-
-    ~handler()
-    {
-        if (_func_data.size()) {
-            _destruct(_func_data.data());
-        }
-    }
+    operator bool() const { return _impl; }
 
 private:
-    exec_type _exec = nullptr;
-    destruct_type _destruct = nullptr;
-    std::vector<uint8_t> _func_data;
+    std::unique_ptr<base> _impl;
 };
 
 } // namespace
