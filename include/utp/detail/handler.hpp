830c45b98010a3e7084fdd3a4d68c99ddcbd7a89
Peter Jankuliak <p.jankuliak@gmail.com>
2019-01-05 20:22:52 +0100

    Create detail/handler::post_exec function
    To post itself using the correct executor

--- include/utp/detail/handler.hpp
+++ include/utp/detail/handler.hpp
@@ -12,18 +12,21 @@ private:
 
     struct base {
         virtual void operator()(const error_code&, Args...) = 0;
+        virtual void post_exec(const error_code&, Args...) = 0;
         virtual ~base() {};
     };
 
-    template<class Executor, class Func>
+    template<class Executor, class Allocator, class Func>
     struct impl final : public base {
         Executor e;
+        Allocator a;
         Func f;
         boost::asio::executor_work_guard<Executor> w;
 
-        template<class E, class F>
-        impl(E&& e, F&& f)
+        template<class E, class A, class F>
+        impl(E&& e, A&& a, F&& f)
             : e(std::forward<E>(e))
+            , a(std::forward<A>(a))
             , f(std::forward<F>(f))
             , w(this->e)
         {}
@@ -31,6 +34,11 @@ private:
         void operator()(const error_code& ec, Args... args) override
         {
             f(ec, args...);
+        }
+
+        void post_exec(const error_code& ec, Args... args) override
+        {
+            e.post(std::bind(std::move(f), ec, args...), a);
         }
     };
 
@@ -48,10 +56,20 @@ public:
 
     template<class Executor, class Func> handler(Executor&& exec, Func&& func)
     {
-        auto e = boost::asio::get_associated_executor(func, exec);
+        namespace net = boost::asio;
 
-        _impl.reset(new impl<decltype(e), Func>( std::move(e)
-                                               , std::forward<Func>(func)));
+        auto e = net::get_associated_executor(func, exec);
+
+        auto a = net::get_associated_allocator
+                   ( func
+                   , std::allocator<void>());
+
+        using impl_t = impl<decltype(e), decltype(a), Func>;
+
+        // XXX: allocate `impl` using `a`
+        _impl = std::make_shared<impl_t>( std::move(e)
+                                        , std::move(a)
+                                        , std::forward<Func>(func));
     }
 
     void operator()(const error_code& ec, Args... args)
@@ -62,6 +80,10 @@ public:
     void operator()(const error_code& ec, Args... args) const
     {
         (*_impl)(ec, args...);
+    }
+
+    void post_exec(const error_code& ec, Args... args) {
+        _impl->post_exec(ec, args...);
     }
 
     operator bool() const { return bool(_impl); }
